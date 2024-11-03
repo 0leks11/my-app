72f712066068ba825661993f4476385a8ac7dfd4
oleksii kozyrev <ak.kozyrev01@gmail.com>
2024-11-03 08:00:53 -0800

    onClick added

--- src/app/components/Squares.tsx
+++ src/app/components/Squares.tsx
@@ -1,8 +1,11 @@
+"use client"
 import React from 'react';
 
 export const Squares = () => {
+    const number = 10;
     return <div className="flex gap-4 justify-center items-center w-96">
-        <div className='size-16 sm:bg-yellow-100 bg-blue-100 '/>
-        <div className='size-16 sm:bg-blue-100 bg-yellow-100 '/>
+        <div onClick={() => {}} className='size-16 sm:bg-yellow-100 bg-blue-100 '/>
+        <div>{number}</div>
+        <div  className='size-16 sm:bg-blue-100 bg-yellow-100 '/>
     </div>
 }
