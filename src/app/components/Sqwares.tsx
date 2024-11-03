fcabf46bd1d32dab619e575e6d7de79d8fc3694e
oleksii kozyrev <ak.kozyrev01@gmail.com>
2024-11-03 02:47:34 -0800

    Sqwares added

---
+++ src/app/components/Sqwares.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Sqwares = () => {
+    return <div className="flex gap-4 justify-center items-center w-96">
+        <div className='size-16 sm:bg-yellow-100 bg-blue-100 '/>
+        <div className='size-16 sm:bg-blue-100 bg-yellow-100 '/>
+    </div>
+}
